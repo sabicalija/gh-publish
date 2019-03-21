e427c3c869f1fbeb8e05b29f561c7c356cbbb07b
Alija Sabic <sabic.alija@gmail.com>
2019-03-21 20:47:03 +0100

    fix(release): add release-notes-generator configuration.
    BREAKING: add semantic-release plugins.
    
     * @semantic-release/release-notes-generator
     * @semantic-release/commit-analyzer

--- release.config.js
+++ release.config.js
@@ -15,6 +15,18 @@ module.exports = {
       {
         assets: [{ path: "test", label: "test file" }, { path: "testdir/*", label: "test folder" }]
       }
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "angular",
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        },
+        writerOpts: {
+          commitsSort: ["subject", "scope"]
+        }
+      }
     ]
   ],
   dryRun: false,
