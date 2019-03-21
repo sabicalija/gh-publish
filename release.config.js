5760b2e82bdaa3163afbb761fa0f88cb04749016
Alija Sabic <sabic.alija@gmail.com>
2019-03-21 20:32:08 +0100

    fix(release): add semantic-release configuration

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  branch: "master",
+  repositoryUrl: "https://github.com/sabicalija/gh-publish",
+  tagFormat: "v${version}",
+  plugins: [
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false
+        // tarballDir: "dist"
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [{ path: "test", label: "test file" }, { path: "testdir", label: "test folder" }]
+      }
+    ]
+  ],
+  dryRun: false,
+  ci: false
+};
